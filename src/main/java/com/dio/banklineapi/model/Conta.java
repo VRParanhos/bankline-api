4a5ccec2ad9de96bc89de11e0486ffc2ee333e66
VRParanhos <vrparanhos@gmail.com>
2022-05-04 20:36:43 -0300

    Adicionando Classes base

---
+++ src/main/java/com/dio/banklineapi/model/Conta.java
@@ -0,0 +1,22 @@
+package com.dio.banklineapi.model;
+
+public class Conta {
+    private long numero;
+    private double saldo;
+
+    public long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
---
+++ src/main/java/com/dio/banklineapi/model/Correntista.java
@@ -0,0 +1,4 @@
+package com.dio.banklineapi.model;
+
+public class Correntista {
+}
