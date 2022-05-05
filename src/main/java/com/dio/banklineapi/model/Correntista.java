6339116e490bd69dc48594dd2b8de5292971f447
VRParanhos <vrparanhos@gmail.com>
2022-05-04 22:07:48 -0300

    Adicionando dependencias

--- src/main/java/com/dio/banklineapi/model/Correntista.java
+++ src/main/java/com/dio/banklineapi/model/Correntista.java
@@ -1,4 +1,46 @@
 package com.dio.banklineapi.model;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
+@Table(name = "tab_correntista")
 public class Correntista {
+    private Integer id;
+    private String cpf;
+    private String nome;
+    private Conta conta;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Conta getConta() {
+        return conta;
+    }
+
+    public void setConta(Conta conta) {
+        this.conta = conta;
+    }
 }
