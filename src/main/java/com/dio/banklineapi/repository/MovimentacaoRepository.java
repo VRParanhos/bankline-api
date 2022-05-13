d8ae4da1280d712f9613a180ac29676d0b1208b1
VICTOR RESENDE PARANHOS <83792564+VRParanhos@users.noreply.github.com>
2022-05-13 00:06:23 -0300

    Adicionando Filtros
    Fazendo alterações no sistema para permitir o filtro por correntista na exibição das movimentações.

--- src/main/java/com/dio/banklineapi/repository/MovimentacaoRepository.java
+++ src/main/java/com/dio/banklineapi/repository/MovimentacaoRepository.java
@@ -1,9 +1,11 @@
 package com.dio.banklineapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dio.banklineapi.model.Movimentacao;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer>{
-
+	public List<Movimentacao>findByIdConta(Integer idConta);
 }
